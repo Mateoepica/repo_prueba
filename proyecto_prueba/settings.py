5ddc55e18d32bf7ed4634330151f0f133f7a5013
LAPTOP-AM7D839K\MateoEPICA <mateo@epica.ai>
2022-05-05 15:02:12 -0500

    allow azure to host

--- proyecto_prueba/settings.py
+++ proyecto_prueba/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=0br1d)=3^s05nie+p-kr*f77p5y=%uhpub^t9z6p2#d*kbg6y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
